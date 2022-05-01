25f46a9fa6e33af2bb52c2cb3e385035a319bd88
Tamim <farman.arefin1@outlook.com>
2022-05-01 16:24:50 +0600

    Added item.js

--- src/App.js
+++ src/App.js
@@ -4,18 +4,20 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 import Footer from "./components/Footer/Footer";
+import About from "./components/About/About";
 
 function App() {
   return (
     <div>
       <Router>
         <Header> </Header>
-
         <Switch>
           <Route exact path="/">
             <Home></Home>
           </Route>
-
+          <Route path="/about">
+            <About></About>
+          </Route>
           <Route path="/home">
             <Home></Home>
           </Route>
