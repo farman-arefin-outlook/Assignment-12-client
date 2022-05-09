ff81b24764910653e226120c1ca6e690b8280e71
Tamim <farman.arefin1@outlook.com>
2022-05-09 22:07:55 +0600

    useItems declare

---
+++ src/hooks/useItems.js
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+
+const useItems = () => {
+    const [items, setItems] = useState([]);
+    const [totalPage, setTotalPage] = useState(0);
+    const [currentPage, setCurrentPage] = useState(0);
+    const size = 9;
+    useEffect(() => {
+        fetch(
+            `https://travel-reach.herokuapp.com/courses?size=${size}&&page=${currentPage}`
+        )
+            .then((res) => res.json())
+            .then((data) => {
+                setItems(data.items);
+                const totalData = data.count;
+                const pages = Math.ceil(totalData / size);
+                setTotalPage(pages);
+            });
+    }, [currentPage]);
+    return { items, setItems, totalPage, currentPage, setCurrentPage };
+};
+
+export default useItems;
